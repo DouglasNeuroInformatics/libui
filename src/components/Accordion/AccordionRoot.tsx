f42b4ddf1f8843e042049371d120ed7e126db2ba
joshunrau <contact@joshuaunrau.com>
2024-03-13 14:01:52 -0400

    refactor: add accordion root

---
+++ src/components/Accordion/AccordionRoot.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+import * as AccordionPrimitive from '@radix-ui/react-accordion';
+
+export const AccordionRoot = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Root>
+>(function AccordionRoot(props, ref) {
+  return <AccordionPrimitive.Root data-testid="accordion" ref={ref} {...props} />;
+});
