a1a9b33fef21858a4c734a45e385a02ee55da284
joshunrau <contact@joshuaunrau.com>
2024-03-13 13:09:52 -0400

    ci: add changelog title

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ export default {
   plugins: [
     ['@semantic-release/commit-analyzer', { preset }],
     ['@semantic-release/release-notes-generator', { preset }],
-    '@semantic-release/changelog',
+    ['@semantic-release/changelog', { changelogTitle: 'Changelog' }],
     [
       '@semantic-release/npm',
       {
