2b22337ef95d1be9c5e82417a757e0659461a16d
joshunrau <contact@joshuaunrau.com>
2024-03-14 15:15:50 -0400

    feat: add radio group

---
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
+
+import { cn } from '@/utils';
+
+import { RadioGroupItem } from './RadioGroupItem';
+
+type RadioGroupProps = React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>;
+
+const RadioGroupRoot = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(function RadioGroup({ className, ...props }, ref) {
+  return (
+    <RadioGroupPrimitive.Root className={cn('grid gap-2', className)} data-testid="radio-group" {...props} ref={ref} />
+  );
+});
+
+export const RadioGroup = Object.assign(RadioGroupRoot, {
+  Item: RadioGroupItem
+});
+
+export type { RadioGroupProps };
