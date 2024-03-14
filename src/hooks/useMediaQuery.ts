9640e2d5ed1a65d399e567ccf7c0f5012f05d758
joshunrau <contact@joshuaunrau.com>
2024-03-13 21:07:06 -0400

    fix: remove deprecated calls

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -30,20 +30,10 @@ export function useMediaQuery(query: string): boolean {
     // Triggered at the first client-side load and if query changes
     handleChange();
 
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (matchMedia.addListener) {
-      matchMedia.addListener(handleChange);
-    } else {
-      matchMedia.addEventListener('change', handleChange);
-    }
+    matchMedia.addEventListener('change', handleChange);
 
     return () => {
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      if (matchMedia.removeListener) {
-        matchMedia.removeListener(handleChange);
-      } else {
-        matchMedia.removeEventListener('change', handleChange);
-      }
+      matchMedia.removeEventListener('change', handleChange);
     };
   }, [query]);
 
