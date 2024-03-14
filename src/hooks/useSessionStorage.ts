46e8cff73766144f976b66931ca66558086d90cb
joshunrau <contact@joshuaunrau.com>
2024-03-14 09:01:39 -0400

    feat: add options to use session storage

--- src/hooks/useSessionStorage.ts
+++ src/hooks/useSessionStorage.ts
@@ -1,6 +1,5 @@
-import { type Dispatch, type SetStateAction, useCallback, useEffect, useState } from 'react';
-
-import { isBrowser } from '@/utils';
+import { useCallback, useEffect, useState } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
 
 import { useEventCallback } from './useEventCallback';
 import { useEventListener } from './useEventListener';
@@ -12,59 +11,128 @@ declare global {
   }
 }
 
-type SetValue<T> = Dispatch<SetStateAction<T>>;
+/**
+ * Represents the options for customizing the behavior of serialization and deserialization.
+ * @template T - The type of the state to be stored in session storage.
+ */
+type UseSessionStorageOptions<T> = {
+  /** A function to deserialize the stored value. */
+  deserializer?: (value: string) => T;
+  /**
+   * If `true` (default), the hook will initialize reading the session storage. In SSR, you should set it to `false`, returning the initial value initially.
+   * @default true
+   */
+  initializeWithValue?: boolean;
+  /** A function to serialize the value before storing it. */
+  serializer?: (value: T) => string;
+};
 
-// A wrapper for "JSON.parse()"" to support "undefined" value
-function parseJSON<T>(value: null | string): T | undefined {
-  try {
-    return value === 'undefined' ? undefined : (JSON.parse(value ?? '') as T);
-  } catch {
-    return undefined;
-  }
-}
+const IS_SERVER = typeof window === 'undefined';
 
-export function useSessionStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
+/**
+ * Custom hook that uses session storage to persist state across page reloads.
+ * @template T - The type of the state to be stored in session storage.
+ * @param {string} key - The key under which the value will be stored in session storage.
+ * @param {T | (() => T)} initialValue - The initial value of the state or a function that returns the initial value.
+ * @param {?UseSessionStorageOptions<T>} [options] - Options for customizing the behavior of serialization and deserialization (optional).
+ * @returns {[T, Dispatch<SetStateAction<T>>]} A tuple containing the stored value and a function to set the value.
+ * @public
+ * @example
+ * ```tsx
+ * const [count, setCount] = useSessionStorage('count', 0);
+ * // Access the `count` value and the `setCount` function to update it.
+ * ```
+ */
+export function useSessionStorage<T>(
+  key: string,
+  initialValue: (() => T) | T,
+  options: UseSessionStorageOptions<T> = {}
+): [T, Dispatch<SetStateAction<T>>] {
+  const { initializeWithValue = true } = options;
+
+  const serializer = useCallback<(value: T) => string>(
+    (value) => {
+      if (options.serializer) {
+        return options.serializer(value);
+      }
+
+      return JSON.stringify(value);
+    },
+    [options]
+  );
+
+  const deserializer = useCallback<(value: string) => T>(
+    (value) => {
+      if (options.deserializer) {
+        return options.deserializer(value);
+      }
+      // Support 'undefined' as a value
+      if (value === 'undefined') {
+        return undefined as unknown as T;
+      }
+
+      const defaultValue = initialValue instanceof Function ? initialValue() : initialValue;
+
+      let parsed: unknown;
+      try {
+        parsed = JSON.parse(value);
+      } catch (error) {
+        console.error('Error parsing JSON:', error);
+        return defaultValue; // Return initialValue if parsing fails
+      }
+
+      return parsed as T;
+    },
+    [options, initialValue]
+  );
+
   // Get from session storage then
   // parse stored json or return initialValue
   const readValue = useCallback((): T => {
+    const initialValueToUse = initialValue instanceof Function ? initialValue() : initialValue;
+
     // Prevent build error "window is undefined" but keep keep working
-    if (!isBrowser()) {
-      return initialValue;
+    if (IS_SERVER) {
+      return initialValueToUse;
     }
 
     try {
-      const item = window.sessionStorage.getItem(key);
-      return item ? (parseJSON(item) as T) : initialValue;
+      const raw = window.sessionStorage.getItem(key);
+      return raw ? deserializer(raw) : initialValueToUse;
     } catch (error) {
       console.warn(`Error reading sessionStorage key “${key}”:`, error);
-      return initialValue;
+      return initialValueToUse;
     }
-  }, [initialValue, key]);
+  }, [initialValue, key, deserializer]);
 
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
-  const [storedValue, setStoredValue] = useState<T>(readValue);
+  const [storedValue, setStoredValue] = useState(() => {
+    if (initializeWithValue) {
+      return readValue();
+    }
+
+    return initialValue instanceof Function ? initialValue() : initialValue;
+  });
 
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to sessionStorage.
-  const setValue: SetValue<T> = useEventCallback((value) => {
+  const setValue: Dispatch<SetStateAction<T>> = useEventCallback((value) => {
     // Prevent build error "window is undefined" but keeps working
-    if (!isBrowser()) {
+    if (IS_SERVER) {
       console.warn(`Tried setting sessionStorage key “${key}” even though environment is not a client`);
     }
 
     try {
       // Allow value to be a function so we have the same API as useState
-      const newValue = value instanceof Function ? value(storedValue) : value;
+      const newValue = value instanceof Function ? value(readValue()) : value;
 
       // Save to session storage
-      window.sessionStorage.setItem(key, JSON.stringify(newValue));
+      window.sessionStorage.setItem(key, serializer(newValue));
 
       // Save state
       setStoredValue(newValue);
 
-      // We dispatch a custom event so every useSessionStorage hook are notified
-      window.dispatchEvent(new Event('session-storage'));
+      // We dispatch a custom event so every similar useSessionStorage hook is notified
+      window.dispatchEvent(new StorageEvent('session-storage', { key }));
     } catch (error) {
       console.warn(`Error setting sessionStorage key “${key}”:`, error);
     }
@@ -72,7 +140,7 @@ export function useSessionStorage<T>(key: string, initialValue: T): [T, SetValue
 
   useEffect(() => {
     setStoredValue(readValue());
-  }, []);
+  }, [key]);
 
   const handleStorageChange = useCallback(
     (event: CustomEvent | StorageEvent) => {
@@ -87,7 +155,7 @@ export function useSessionStorage<T>(key: string, initialValue: T): [T, SetValue
   // this only works for other documents, not the current one
   useEventListener('storage', handleStorageChange);
 
-  // this is a custom event, triggered in writeValueTosessionStorage
+  // this is a custom event, triggered in writeValueToSessionStorage
   // See: useSessionStorage()
   useEventListener('session-storage', handleStorageChange);
 
