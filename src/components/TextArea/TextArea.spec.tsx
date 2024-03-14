41f6fa49155bd3abeb62dd4767e360e9dc0cccd0
joshunrau <contact@joshuaunrau.com>
2024-03-14 16:16:14 -0400

    feat: add text area

---
+++ src/components/TextArea/TextArea.spec.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import { describe, expect, it } from 'vitest';
+
+import { TextArea } from './TextArea';
+
+const TEST_ID = 'text-area';
+
+describe('TextArea ', () => {
+  it('should render', async () => {
+    render(<TextArea />);
+    const input = screen.getByTestId(TEST_ID);
+    expect(input).toBeInTheDocument();
+  });
+  it('should allow typing', async () => {
+    render(<TextArea />);
+    const input = screen.getByTestId(TEST_ID);
+    await userEvent.type(input, 'hello world');
+    expect(input).toHaveDisplayValue('hello world');
+  });
+});
