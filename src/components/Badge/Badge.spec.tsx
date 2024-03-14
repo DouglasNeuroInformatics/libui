a43d4920669c1bac23acf0fc22cb7e849eae403a
joshunrau <contact@joshuaunrau.com>
2024-03-14 15:43:29 -0400

    feat: add badge

---
+++ src/components/Badge/Badge.spec.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { Badge } from './Badge';
+
+const TEST_ID = 'badge';
+
+describe('Badge', () => {
+  it('should render', async () => {
+    render(<Badge />);
+    expect(screen.getByTestId(TEST_ID)).toBeInTheDocument();
+  });
+  it('should contain a custom class name', () => {
+    render(<Badge className="foo" />);
+    expect(screen.getByTestId(TEST_ID)).toHaveClass('foo');
+  });
+});
