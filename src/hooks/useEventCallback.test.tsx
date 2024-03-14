9ed7258ae3827db0ece7cd004418d47a30536bed
joshunrau <contact@joshuaunrau.com>
2024-03-13 20:15:40 -0400

    test: add event callback test

---
+++ src/hooks/useEventCallback.test.tsx
@@ -0,0 +1,20 @@
+import { fireEvent, render, renderHook, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+
+import { useEventCallback } from './useEventCallback';
+
+describe('useEventCallback', () => {
+  it('should not call the callback during render', () => {
+    const fn = vi.fn();
+    const { result } = renderHook(() => useEventCallback(fn));
+    render(<button onClick={result.current}>Click me</button>);
+    expect(fn).not.toHaveBeenCalled();
+  });
+  it('should call the callback when the event is triggered', () => {
+    const fn = vi.fn();
+    const { result } = renderHook(() => useEventCallback(fn));
+    render(<button onClick={result.current}>Click me</button>);
+    fireEvent.click(screen.getByText('Click me'));
+    expect(fn).toHaveBeenCalled();
+  });
+});
