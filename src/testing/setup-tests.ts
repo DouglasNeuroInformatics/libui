5966eb3cac3cee3355b137de739121761f63bcdc
joshunrau <contact@joshuaunrau.com>
2024-03-14 09:00:59 -0400

    test: add mocks and revert strict mode

--- src/testing/setup-tests.ts
+++ src/testing/setup-tests.ts
@@ -1,9 +1,7 @@
-import { cleanup, configure } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
 
 import '@testing-library/jest-dom/vitest';
-
-configure({ reactStrictMode: true });
 
 // Since we're not using vitest globals, we need to explicitly call cleanup()
 // for testing-library. See:
