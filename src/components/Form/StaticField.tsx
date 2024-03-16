6445cd21d67d080fa7f475bd69ee27a9744b8e70
joshunrau <contact@joshuaunrau.com>
2024-03-16 15:51:46 -0400

    feat: add static field

---
+++ src/components/Form/StaticField.tsx
@@ -0,0 +1,70 @@
+import { useCallback } from 'react';
+
+import type {
+  ArrayFieldValue,
+  ArrayFormField,
+  FormDataType,
+  FormFieldValue,
+  PartialFormDataType,
+  PrimitiveFieldValue
+} from '@douglasneuroinformatics/libui-form-types';
+
+import { ArrayField } from './ArrayField';
+import { PrimitiveField, type PrimitiveFieldProps } from './PrimitiveField';
+
+import type { FieldError, FormErrors } from './types';
+
+export type StaticFieldProps<TData extends FormDataType> = {
+  errors: FormErrors<TData>;
+  field: ArrayFormField | PrimitiveFieldProps['field'];
+  name: string;
+  setErrors: React.Dispatch<React.SetStateAction<FormErrors<TData>>>;
+  setValues: React.Dispatch<React.SetStateAction<PartialFormDataType<TData>>>;
+  values: PartialFormDataType<TData>;
+};
+
+export const StaticField = <TData extends FormDataType>({
+  errors,
+  field,
+  name,
+  setErrors,
+  setValues,
+  values
+}: StaticFieldProps<TData>) => {
+  const setError = useCallback(
+    <TValue extends FormFieldValue>(error: FieldError<TValue>) => {
+      return setErrors((prevErrors) => ({ ...prevErrors, [name]: error }));
+    },
+    [setErrors]
+  );
+
+  const setValue = useCallback(
+    <TValue extends FormFieldValue>(value: TValue) => {
+      return setValues((prevValues) => ({ ...prevValues, [name]: value }));
+    },
+    [setValues]
+  );
+
+  if (field.kind === 'array') {
+    return (
+      <ArrayField
+        {...field}
+        error={errors[name] as Record<string, string>[]}
+        name={name}
+        setError={setError}
+        setValue={setValue}
+        value={values[name] as ArrayFieldValue}
+      />
+    );
+  }
+  return (
+    <PrimitiveField
+      error={errors[name] as string}
+      field={field}
+      name={name}
+      setError={setError}
+      setValue={setValue}
+      value={values[name] as PrimitiveFieldValue}
+    />
+  );
+};
