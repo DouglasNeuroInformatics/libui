69f0f9d16f573017b8a808a0e821910b3317fcef
joshunrau <contact@joshuaunrau.com>
2024-03-16 15:58:28 -0400

    feat: add form fields component

---
+++ src/components/Form/FormFieldsComponent.tsx
@@ -0,0 +1,25 @@
+import type { FormDataType, FormFields, PartialFormDataType } from '@douglasneuroinformatics/libui-form-types';
+
+import { DynamicField } from './DynamicField';
+import { StaticField } from './StaticField';
+
+import type { FormErrors } from './types';
+
+export type FormFieldsComponentProps<T extends FormDataType> = {
+  errors: FormErrors<T>;
+  fields: FormFields<T>;
+  setErrors: React.Dispatch<React.SetStateAction<FormErrors<T>>>;
+  setValues: React.Dispatch<React.SetStateAction<PartialFormDataType<T>>>;
+  values: PartialFormDataType<T>;
+};
+
+/** Renders an object containing key value pairs, where the value is a FormField of some kind */
+export const FormFieldsComponent = <T extends FormDataType>({ fields, ...props }: FormFieldsComponentProps<T>) => {
+  return Object.keys(fields).map((name) => {
+    const field = fields[name]!;
+    if (field.kind === 'dynamic') {
+      return <DynamicField {...props} field={field} key={name} name={name} />;
+    }
+    return <StaticField {...props} field={field} key={name} name={name} />;
+  });
+};
