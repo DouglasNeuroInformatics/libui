179d33a33d3163e4a4fa21b6a02f9a2f24f05511
joshunrau <contact@joshuaunrau.com>
2024-03-16 15:03:13 -0400

    feat: add slider

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Range, Root, Thumb, Track } from '@radix-ui/react-slider';
+
+import { cn } from '@/utils';
+
+export const Slider = React.forwardRef<React.ElementRef<typeof Root>, React.ComponentPropsWithoutRef<typeof Root>>(
+  function Slider({ className, ...props }, ref) {
+    return (
+      <Root className={cn('relative flex w-full touch-none select-none items-center', className)} ref={ref} {...props}>
+        <Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary opacity-20">
+          <Range className="absolute h-full bg-primary" />
+        </Track>
+        <Thumb className="block h-4 w-4 rounded-full border border-slate-500 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+      </Root>
+    );
+  }
+);
