36cd86714fac82ea82c468e2312a1896ad4d7f5b
joshunrau <contact@joshuaunrau.com>
2024-03-16 13:56:28 -0400

    fix: set autocomplete to off

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -10,6 +10,7 @@ export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(fun
 ) {
   return (
     <textarea
+      autoComplete="off"
       className={cn(
         'flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
         className
