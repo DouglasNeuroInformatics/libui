8495f34b379cf76831943198e48771d392cec9b5
joshunrau <contact@joshuaunrau.com>
2024-03-17 14:53:56 -0400

    style: use synthetic default import for react

--- src/components/ScrollArea/ScrollBar.tsx
+++ src/components/ScrollArea/ScrollBar.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import { ScrollAreaScrollbar, ScrollAreaThumb } from '@radix-ui/react-scroll-area';
 
--- src/components/Tooltip/TooltipContent.tsx
+++ src/components/Tooltip/TooltipContent.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import { Content } from '@radix-ui/react-tooltip';
 
