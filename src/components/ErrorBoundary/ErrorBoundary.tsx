922d6d3c57a1a54415febb07235c1a78f352f942
joshunrau <contact@joshuaunrau.com>
2024-03-17 13:54:32 -0400

    feat: add error fallback and boundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,7 @@
+import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
+
+import { ErrorFallback } from '../ErrorFallback';
+
+export const ErrorBoundary: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <ReactErrorBoundary FallbackComponent={ErrorFallback}>{children}</ReactErrorBoundary>;
+};
