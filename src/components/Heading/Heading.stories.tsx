431611a704b8c2382e9aedc1235368eec1521608
joshunrau <contact@joshuaunrau.com>
2024-03-17 14:02:15 -0400

    feat: add heading

---
+++ src/components/Heading/Heading.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Heading } from './Heading';
+
+type Story = StoryObj<typeof Heading>;
+
+const exampleText = 'Whereas disregard and contempt for human rights have resulted';
+
+export default { component: Heading } as Meta<typeof Heading>;
+
+export const Heading1: Story = {
+  args: {
+    children: exampleText,
+    variant: 'h1'
+  }
+};
+
+export const Heading2: Story = {
+  args: {
+    children: exampleText,
+    variant: 'h2'
+  }
+};
+
+export const Heading3: Story = {
+  args: {
+    children: exampleText,
+    variant: 'h3'
+  }
+};
+
+export const Heading4: Story = {
+  args: {
+    children: exampleText,
+    variant: 'h4'
+  }
+};
---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@/utils';
+
+export type HeadingProps = {
+  children: string;
+  className?: string;
+  variant: 'h1' | 'h2' | 'h3' | 'h4';
+};
+
+export const Heading = ({ children, className, variant }: HeadingProps) => {
+  switch (variant) {
+    case 'h1':
+      return <h1 className={cn('text-3xl font-bold tracking-tight', className)}>{children}</h1>;
+    case 'h2':
+      return <h2 className={cn('text-2xl font-semibold tracking-tight', className)}>{children}</h2>;
+    case 'h3':
+      return <h3 className={cn('text-xl font-semibold tracking-tight', className)}>{children}</h3>;
+    case 'h4':
+      return <h4 className={cn('text-lg font-semibold tracking-tight', className)}>{children}</h4>;
+    default:
+      throw new Error(`Unhandled heading variant: ${variant satisfies never}`);
+  }
+};
