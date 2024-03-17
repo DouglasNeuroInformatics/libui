5a3dc022df4ac2eded34b070be3b429111f70795
joshunrau <contact@joshuaunrau.com>
2024-03-17 15:21:48 -0400

    feat: add progress

---
+++ src/components/Progress/Progress.stories.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Progress } from './Progress';
+
+type Story = StoryObj<typeof Progress>;
+
+export default { component: Progress } as Meta<typeof Progress>;
+
+export const Default: Story = {
+  decorators: [
+    (Story) => {
+      const [progress, setProgress] = useState(13);
+
+      useEffect(() => {
+        const timer = setTimeout(() => setProgress(66), 500);
+        return () => clearTimeout(timer);
+      }, []);
+
+      return <Story args={{ className: 'w-[60%]', value: progress }} />;
+    }
+  ]
+};
