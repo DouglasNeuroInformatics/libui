ec786b77a859b4e3ba217c0571fc02eec3450442
joshunrau <contact@joshuaunrau.com>
2024-03-18 10:47:48 -0400

    fix: reduce rounding of badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -5,7 +5,7 @@ import { type VariantProps, cva } from 'class-variance-authority';
 import { cn } from '@/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-sm border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     defaultVariants: {
       variant: 'default'
