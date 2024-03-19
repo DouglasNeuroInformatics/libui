05ea461af7ca51df3c1ebaf7ce854becd15c2cff
joshunrau <contact@joshuaunrau.com>
2024-03-19 13:20:34 -0400

    fix: update styling for composite field

--- src/components/Form/CompositeField.tsx
+++ src/components/Form/CompositeField.tsx
@@ -5,9 +5,11 @@ import { MinusCircleIcon, PlusCircleIcon } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
 import { Button } from '../Button';
+import { Label } from '../Label';
 import { ScalarField } from './ScalarField';
 
 import type { BaseFieldComponentProps } from './types';
+import { Heading } from '../Heading';
 
 export type CompositeFieldProps = BaseFieldComponentProps<CompositeFieldValue> & CompositeFormField;
 
@@ -43,41 +45,44 @@ export const CompositeField = memo(function CompositeField({
   };
 
   return (
-    <div className="space-y-8">
-      {arrayValue.map((fields, i) => (
-        <div className="space-y-4" key={i}>
-          <span className="font-medium text-muted-foreground">{label + ' ' + (i + 1)}</span>
-          {Object.keys(fields).map((name) => {
-            const field = fieldset[name];
-            const fieldProps = field?.kind === 'dynamic' ? field.render(fields) : field;
-            if (!fieldProps) {
-              return null;
-            }
-            return (
-              <ScalarField
-                error={arrayError?.[i]?.[name]}
-                field={fieldProps}
-                key={name}
-                name={name}
-                setError={(error) => {
-                  const newArrayError = arrayError ? [...arrayError] : [];
-                  if (!newArrayError[i]) {
-                    newArrayError[i] = {};
-                  }
-                  newArrayError[i]![name] = error;
-                  setArrayError(newArrayError);
-                }}
-                setValue={(value) => {
-                  const newArrayValue = [...arrayValue];
-                  newArrayValue[i]![name] = value;
-                  setArrayValue(newArrayValue);
-                }}
-                value={arrayValue?.[i]?.[name]}
-              />
-            );
-          })}
-        </div>
-      ))}
+    <div className="space-y-4">
+      <Heading variant="h5">{label}</Heading>
+      <div className="space-y-8">
+        {arrayValue.map((fields, i) => (
+          <div className="space-y-4" key={i}>
+            <Label className="font-semibold italic">{label + ' ' + (i + 1)}</Label>
+            {Object.keys(fields).map((name) => {
+              const field = fieldset[name];
+              const fieldProps = field?.kind === 'dynamic' ? field.render(fields) : field;
+              if (!fieldProps) {
+                return null;
+              }
+              return (
+                <ScalarField
+                  error={arrayError?.[i]?.[name]}
+                  field={fieldProps}
+                  key={name}
+                  name={name}
+                  setError={(error) => {
+                    const newArrayError = arrayError ? [...arrayError] : [];
+                    if (!newArrayError[i]) {
+                      newArrayError[i] = {};
+                    }
+                    newArrayError[i]![name] = error;
+                    setArrayError(newArrayError);
+                  }}
+                  setValue={(value) => {
+                    const newArrayValue = [...arrayValue];
+                    newArrayValue[i]![name] = value;
+                    setArrayValue(newArrayValue);
+                  }}
+                  value={arrayValue?.[i]?.[name]}
+                />
+              );
+            })}
+          </div>
+        ))}
+      </div>
       <div className="flex gap-3">
         <Button type="button" variant="outline" onClick={appendField}>
           {t('form.append')}
