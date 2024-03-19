fcf9d79b5ac9036831beaf218117ecd5f6ed2e02
joshunrau <contact@joshuaunrau.com>
2024-03-19 15:59:55 -0400

    fix: make radio group item circle

--- src/components/RadioGroup/RadioGroupItem.tsx
+++ src/components/RadioGroup/RadioGroupItem.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
-import { CheckIcon } from 'lucide-react';
+import { CircleIcon } from 'lucide-react';
 
 import { cn } from '@/utils';
 
-export const RadioGroupItem = ({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>) => {
+export const RadioGroupItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(function RadioGroupItem({ className, ...props }, ref) {
   return (
     <RadioGroupPrimitive.Item
       className={cn(
-        'h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+        'flex aspect-square h-4 w-4 items-center justify-center rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
         className
       )}
+      ref={ref}
       {...props}
     >
-      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <CheckIcon className="h-4 w-4" />
+      <RadioGroupPrimitive.Indicator asChild>
+        <CircleIcon
+          className="fill-current text-current"
+          style={{ height: '0.625rem', strokeWidth: '2px', width: '0.625rem' }}
+        />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   );
-};
+});
