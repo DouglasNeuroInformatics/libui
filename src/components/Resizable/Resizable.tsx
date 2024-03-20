ad351ebb403974a5e664c6c260a32eda8a232af4
joshunrau <contact@joshuaunrau.com>
2024-03-20 19:33:30 -0400

    fix: fix typescript error

--- src/components/Resizable/Resizable.tsx
+++ src/components/Resizable/Resizable.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
-import { Panel } from 'react-resizable-panels';
+import { Panel, type PanelProps } from 'react-resizable-panels';
 
-import { ResizableHandle } from './ResizableHandle';
-import { ResizablePanelGroup } from './ResizablePanelGroup';
+import { ResizableHandle, type ResizableHandleProps } from './ResizableHandle';
+import { ResizablePanelGroup, type ResizablePanelGroupProps } from './ResizablePanelGroup';
+
+type ResizableRootType = React.FC<{ children: React.ReactNode }>;
+type ResizableType = ResizableRootType & {
+  Handle: React.FC<ResizableHandleProps>;
+  Panel: React.FC<PanelProps>;
+  PanelGroup: React.FC<ResizablePanelGroupProps>;
+};
 
 // This is only for storybook and is unnecessary for real-world use
-const ResizableRoot: React.FC<{ children: React.ReactNode }> = ({ children }) => <>{children}</>;
+const ResizableRoot: ResizableRootType = ({ children }) => <>{children}</>;
 
-export const Resizable = Object.assign(ResizableRoot, {
+export const Resizable: ResizableType = Object.assign(ResizableRoot, {
   Handle: ResizableHandle,
   Panel,
   PanelGroup: ResizablePanelGroup
