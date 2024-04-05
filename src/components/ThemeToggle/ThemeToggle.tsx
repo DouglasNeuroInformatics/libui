afa5058c17485d5e7f39849eb9511f0b3bc321ab
joshunrau <contact@joshuaunrau.com>
2024-04-05 13:32:07 -0400

    fix: passthrough button props to theme toggle

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -4,11 +4,9 @@ import { useTheme } from '@/hooks/useTheme';
 
 import { Button, type ButtonProps } from '../Button';
 
-export type ThemeToggleProps = {
-  variant?: ButtonProps['variant'];
-};
+export type ThemeToggleProps = Omit<ButtonProps, 'children' | 'size'>;
 
-export const ThemeToggle = ({ variant = 'outline' }: ThemeToggleProps) => {
+export const ThemeToggle = ({ onClick, variant = 'outline', ...props }: ThemeToggleProps) => {
   const [theme, setTheme] = useTheme();
 
   const toggleTheme = () => {
@@ -16,7 +14,15 @@ export const ThemeToggle = ({ variant = 'outline' }: ThemeToggleProps) => {
   };
 
   return (
-    <Button size="icon" variant={variant} onClick={toggleTheme}>
+    <Button
+      size="icon"
+      variant={variant}
+      onClick={(event) => {
+        toggleTheme();
+        onClick?.(event);
+      }}
+      {...props}
+    >
       <SunIcon className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
