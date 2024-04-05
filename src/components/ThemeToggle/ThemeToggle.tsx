09caf38c8a85baab0a98496623e4b2adfdae84af
joshunrau <contact@joshuaunrau.com>
2024-04-05 13:07:38 -0400

    feat: add variant option to ThemeToggle

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -2,13 +2,13 @@ import { MoonIcon, SunIcon } from 'lucide-react';
 
 import { useTheme } from '@/hooks/useTheme';
 
-import { Button } from '../Button';
+import { Button, type ButtonProps } from '../Button';
 
 export type ThemeToggleProps = {
-  align?: 'center' | 'end' | 'start';
+  variant?: ButtonProps['variant'];
 };
 
-export const ThemeToggle = () => {
+export const ThemeToggle = ({ variant = 'outline' }: ThemeToggleProps) => {
   const [theme, setTheme] = useTheme();
 
   const toggleTheme = () => {
@@ -16,7 +16,7 @@ export const ThemeToggle = () => {
   };
 
   return (
-    <Button size="icon" variant="outline" onClick={toggleTheme}>
+    <Button size="icon" variant={variant} onClick={toggleTheme}>
       <SunIcon className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
