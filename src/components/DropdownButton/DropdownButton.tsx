81dbb1124861800f96da9cb72bf568a0ac159894
joshunrau <contact@joshuaunrau.com>
2024-05-07 16:19:56 -0400

    fix: set gap for action dropdown

--- src/components/DropdownButton/DropdownButton.tsx
+++ src/components/DropdownButton/DropdownButton.tsx
@@ -9,7 +9,7 @@ export const DropdownButton = React.forwardRef<HTMLButtonElement, React.ButtonHT
     return (
       <button
         className={cn(
-          'flex h-9 w-full items-center whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+          'flex h-9 w-full items-center gap-2 whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
           children ? 'justify-between' : 'justify-end',
           className
         )}
