79435cbcca1d1e8832264b46bcf579c7dde223cd
David Roper <davidroper321@gmail.com>
2024-09-10 14:04:52 -0400

    test: add tests ti filedropzone

---
+++ src/components/FileDropzone/FileDropzone.spec.tsx
@@ -0,0 +1,50 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { FileDropzone } from './FileDropzone';
+
+describe('FileDropzone', () => {
+  const testfile = new File([new Blob()], 'testfile.txt');
+  it('should render', () => {
+    render(
+      <FileDropzone
+        file={null}
+        setFile={function (file: File): void {
+          throw new Error('Function not implemented. File name is ' + file.name);
+        }}
+      />
+    );
+    expect(screen.getByText("Drag'n'drop files or click on box to upload")).toBeInTheDocument();
+  });
+  it('should have file', () => {
+    render(
+      <FileDropzone
+        file={testfile}
+        setFile={function (file: File): void {
+          throw new Error('Function not implemented. File name is ' + file.name);
+        }}
+      />
+    );
+    expect(screen.getByText('testfile.txt')).toBeInTheDocument();
+  });
+  it('drag active should work', () => {
+    render(
+      <FileDropzone
+        file={null}
+        setFile={function (file: File): void {
+          throw new Error('Function not implemented. File name is ' + file.name);
+        }}
+      />
+    );
+
+    const fileDropzoneElement = screen.getByText("Drag'n'drop files or click on box to upload");
+
+    fireEvent.dragEnter(fileDropzoneElement, {
+      dataTransfer: {
+        files: [testfile]
+      }
+    });
+
+    expect(screen.getByText('File to upload')).toBeInTheDocument();
+  });
+});
