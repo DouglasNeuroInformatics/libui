f466fb3e425d42982bc4e804419452e0697b45b0
joshunrau <contact@joshuaunrau.com>
2024-09-17 11:59:12 -0400

    test: add mockTranslationStore

--- src/testing/setup-tests.ts
+++ src/testing/setup-tests.ts
@@ -1,9 +1,12 @@
 import { cleanup } from '@testing-library/react';
 import { afterEach, vi } from 'vitest';
 
+import { mockTranslationStore } from './mocks';
+
 import '@testing-library/jest-dom/vitest';
 
 vi.mock('zustand');
+mockTranslationStore();
 
 // Since we're not using vitest globals, we need to explicitly call cleanup()
 // for testing-library. See:
