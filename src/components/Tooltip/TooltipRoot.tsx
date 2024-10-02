b1d13a99e32eb88e47871ea13fcd18ec639c204a
joshunrau <contact@joshuaunrau.com>
2024-10-02 12:26:16 -0400

    fix: add controlled option to tooltip

--- src/components/Tooltip/TooltipRoot.tsx
+++ src/components/Tooltip/TooltipRoot.tsx
@@ -7,14 +7,26 @@ export type TooltipRootProps = {
   children: React.ReactNode;
   /** The duration from when the mouse enters a tooltip trigger until the tooltip opens. */
   delayDuration?: number;
+  /** Event handler called when the open state of the tooltip changes. */
+  onOpenChange?: (open: boolean) => void;
+  /** The controlled open state of the tooltip. Must be used in conjunction with onOpenChange. */
+  open?: boolean;
   /** How much time a user has to enter another trigger without incurring a delay again. */
   skipDelayDuration?: number;
 };
 
-export const TooltipRoot = ({ children, delayDuration = 0, skipDelayDuration = 300 }: TooltipRootProps) => {
+export const TooltipRoot = ({
+  children,
+  delayDuration = 0,
+  onOpenChange,
+  open,
+  skipDelayDuration = 300
+}: TooltipRootProps) => {
   return (
     <Provider delayDuration={delayDuration} skipDelayDuration={skipDelayDuration}>
-      <Root>{children}</Root>
+      <Root open={open} onOpenChange={onOpenChange}>
+        {children}
+      </Root>
     </Provider>
   );
 };
