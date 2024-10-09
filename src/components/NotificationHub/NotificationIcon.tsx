f9d4d64c26aaab7bc708547c2644e76267c79697
joshunrau <contact@joshuaunrau.com>
2024-10-09 11:28:58 -0400

    chore: remove heroicons

--- src/components/NotificationHub/NotificationIcon.tsx
+++ src/components/NotificationHub/NotificationIcon.tsx
@@ -1,5 +1,3 @@
-import { CheckCircleIcon, ExclamationCircleIcon, InformationCircleIcon, XCircleIcon } from '@heroicons/react/24/solid';
-
 import { type NotificationInterface } from '@/hooks/useNotificationsStore';
 
 export type NotificationIconProps = {
@@ -9,12 +7,72 @@ export type NotificationIconProps = {
 export const NotificationIcon = ({ type }: NotificationIconProps) => {
   switch (type) {
     case 'error':
-      return <XCircleIcon aria-hidden="true" className="h-6 w-6 text-red-500" />;
+      return (
+        <svg
+          aria-hidden="true"
+          className="h-6 w-6 text-red-500"
+          data-slot="icon"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clipRule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
+            fillRule="evenodd"
+          ></path>
+        </svg>
+      );
     case 'info':
-      return <InformationCircleIcon aria-hidden="true" className="h-6 w-6 text-blue-500" />;
+      return (
+        <svg
+          aria-hidden="true"
+          className="h-6 w-6 text-blue-500"
+          data-slot="icon"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clipRule="evenodd"
+            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+            fillRule="evenodd"
+          ></path>
+        </svg>
+      );
     case 'success':
-      return <CheckCircleIcon aria-hidden="true" className="h-6 w-6 text-green-500" />;
+      return (
+        <svg
+          aria-hidden="true"
+          className="h-6 w-6 text-green-500"
+          data-slot="icon"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clipRule="evenodd"
+            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+            fillRule="evenodd"
+          />
+        </svg>
+      );
     case 'warning':
-      return <ExclamationCircleIcon aria-hidden="true" className="h-6 w-6 text-yellow-500" />;
+      return (
+        <svg
+          aria-hidden="true"
+          className="h-6 w-6 text-yellow-500"
+          data-slot="icon"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clipRule="evenodd"
+            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+            fillRule="evenodd"
+          ></path>
+        </svg>
+      );
   }
 };
