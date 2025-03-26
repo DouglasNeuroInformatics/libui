30cc6e6c78ac10cf0aa631e4a59e5f1c0016c20b
David Roper <davidroper321@gmail.com>
2025-03-26 14:03:04 -0400

    test: add page traversal test to client table test file

--- src/components/ClientTable/ClientTable.spec.tsx
+++ src/components/ClientTable/ClientTable.spec.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
 
 import { ClientTable } from './ClientTable';
-
 const TEST_ID = 'ClientTable';
 const FIRST_BUTTON_ID = 'first-page-button';
 const PREVIOUS_BUTTON_ID = 'previous-page-button';
 const NEXT_BUTTON_ID = 'next-page-button';
 const LAST_BUTTON_ID = 'last-page-button';
-
+const PAGE_NUMBER_ID = 'page-numbers';
 describe('ClientTable', () => {
   it('should render', () => {
     render(<ClientTable columns={[]} data={[]} minRows={10} />);
@@ -21,5 +21,46 @@ describe('ClientTable', () => {
   it('should contain a custom class name', () => {
     render(<ClientTable className="foo" columns={[]} data={[]} minRows={10} />);
     expect(screen.getByTestId(TEST_ID)).toHaveClass('foo');
+  });
+  it('should function correctly', async () => {
+    render(
+      <ClientTable
+        columns={[
+          {
+            field: 'f1',
+            label: 'Field 1'
+          },
+          {
+            field: 'f2',
+            label: 'Field 2'
+          }
+        ]}
+        data={[
+          {
+            f1: 1,
+            f2: 2
+          },
+          {
+            f1: 23,
+            f2: 24
+          }
+        ]}
+        entriesPerPage={1}
+        minRows={3}
+      />
+    );
+    expect(screen.getByTestId(PAGE_NUMBER_ID)).toBeInTheDocument();
+    await userEvent.click(screen.getByTestId(NEXT_BUTTON_ID));
+    await userEvent.click(await screen.findByText('23.00'));
+    expect(screen.getByTestId(PAGE_NUMBER_ID).textContent).toBe('2 - 2 / 2');
+    await userEvent.click(screen.getByTestId(PREVIOUS_BUTTON_ID));
+    await userEvent.click(await screen.findByText('1.00'));
+    expect(screen.getByTestId(PAGE_NUMBER_ID).textContent).toBe('1 - 1 / 2');
+    await userEvent.click(screen.getByTestId(LAST_BUTTON_ID));
+    await userEvent.click(await screen.findByText('23.00'));
+    expect(screen.getByTestId(PAGE_NUMBER_ID).textContent).toBe('2 - 2 / 2');
+    await userEvent.click(screen.getByTestId(FIRST_BUTTON_ID));
+    await userEvent.click(await screen.findByText('1.00'));
+    expect(screen.getByTestId(PAGE_NUMBER_ID).textContent).toBe('1 - 1 / 2');
   });
 });
