8619d1c6dcfbdf10dd49114d65837533d2d11840
David Roper <davidroper321@gmail.com>
2025-03-26 13:04:27 -0400

    test: remove unused code from test

--- src/components/ClientTable/ClientTable.spec.tsx
+++ src/components/ClientTable/ClientTable.spec.tsx
@@ -3,40 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import { ClientTable } from './ClientTable';
-import { type ClientTableColumn } from './ClientTable';
-
-type ExampleItem = {
-  c1: number;
-  c2: number;
-  c3: number;
-  c4: number;
-  c5: number;
-  c6: number;
-  c7: number;
-  c8: number;
-  c9: number;
-  c10: number;
-  c11: number;
-  c12: number;
-  c13: number;
-  c14: number;
-  c15: number;
-  id: string;
-};
-
-const columns: ClientTableColumn<ExampleItem>[] = [
-  {
-    field: 'id',
-    label: 'ID'
-  }
-];
-
-for (let i = 1; i < 16; i++) {
-  columns.push({
-    field: `c${i}` as keyof ExampleItem,
-    label: `Column ${i}`
-  });
-}
 
 const TEST_ID = 'ClientTable';
 
