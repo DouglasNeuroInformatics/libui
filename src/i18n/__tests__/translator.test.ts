d523a982828b97fcd7e86a1d67dd3f735d306135
joshunrau <contact@joshuaunrau.com>
2025-06-07 10:31:15 -0400

    refactor: add Translator

---
+++ src/i18n/__tests__/translator.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+
+import { Translator } from '../translator';
+
+describe('Translator', () => {
+  const translator = new Translator();
+
+  it('should initially not be initialized', () => {
+    expect(translator.isInitialized).toBe(false);
+  });
+
+  it('should not allow accessing the changeLanguage method before initialization', () => {
+    expect(() => translator.changeLanguage('fr')).toThrow(
+      "Cannot access method 'changeLanguage' of Translator instance before initialization"
+    );
+  });
+
+  it('should allow initialization', () => {
+    translator.init({ defaultLanguage: 'en', translations: {} });
+    expect(translator.isInitialized).toBe(true);
+  });
+
+  it('should not allow reinitialization', () => {
+    expect(() => translator.init({ defaultLanguage: 'en', translations: {} })).toThrow(
+      'Cannot reinitialize Translator'
+    );
+  });
+});
---
+++ src/i18n/translator.ts
@@ -0,0 +1,56 @@
+import type { SetOptional } from 'type-fest';
+
+import type { Language, Translations } from './types';
+
+type LanguageChangeHandler = (this: void, language: Language) => void;
+
+type TranslatorConfig = {
+  defaultLanguage?: Language;
+  onLanguageChange?: LanguageChangeHandler;
+  translations: SetOptional<Translations, 'libui'>;
+};
+
+function InitializedOnly<T extends Translator, TArgs extends any[], TReturn>(
+  target: (this: T, ...args: TArgs) => TReturn,
+  context: ClassGetterDecoratorContext<T> | ClassMethodDecoratorContext<T> | ClassSetterDecoratorContext<T>
+) {
+  const name = context.name.toString();
+  function replacementMethod(this: T, ...args: TArgs): TReturn {
+    if (!this.isInitialized) {
+      throw new Error(`Cannot access ${context.kind} '${name}' of Translator instance before initialization`);
+    }
+    return target.call(this, ...args);
+  }
+  return replacementMethod;
+}
+
+export class Translator {
+  #config: null | TranslatorConfig;
+  #resolvedLanguage: Language | null;
+
+  constructor() {
+    this.#config = null;
+    this.#resolvedLanguage = null;
+  }
+
+  get isInitialized() {
+    return this.#config !== null;
+  }
+
+  get resolvedLanguage() {
+    return this.#resolvedLanguage;
+  }
+
+  @InitializedOnly
+  changeLanguage(language: Language) {
+    this.#resolvedLanguage = language;
+  }
+
+  init(config: TranslatorConfig) {
+    if (this.isInitialized) {
+      throw new Error('Cannot reinitialize Translator');
+    }
+    this.#config = config;
+    this.#resolvedLanguage = config.defaultLanguage ?? 'en';
+  }
+}
