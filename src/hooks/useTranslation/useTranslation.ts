8eaebfb66a04484e61e1fb1ac302c12adc50ae4f
joshunrau <contact@joshuaunrau.com>
2025-06-07 15:19:18 -0400

    chore: update

--- src/hooks/useTranslation/useTranslation.ts
+++ src/hooks/useTranslation/useTranslation.ts
@@ -1,8 +1,9 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
+import type { Primitive } from 'type-fest';
 import { useStore } from 'zustand';
 
-import type { Language, TranslateFunction, TranslationNamespace } from '@/i18n';
+import type { Language, TranslateFunction, TranslationKey, TranslationNamespace } from '@/i18n';
 
 // this is required since our storybook manager plugin cannot use vite aliases
 import { i18n } from '../../i18n';
@@ -11,13 +12,18 @@ export function useTranslation<TNamespace extends TranslationNamespace | undefin
   namespace?: TNamespace
 ) {
   const [resolvedLanguage, setResolvedLanguage] = useState(i18n.resolvedLanguage);
-  const { changeLanguage, t } = useMemo(
-    () => ({
+  const { changeLanguage, t } = useMemo(() => {
+    const _t = i18n.t.bind(i18n);
+    return {
       changeLanguage: i18n.changeLanguage.bind(i18n),
-      t: i18n.t.bind(i18n)
-    }),
-    []
-  );
+      t: (target: TranslationKey<TNamespace> | { [L in Language]?: string }, ...args: Exclude<Primitive, symbol>[]) => {
+        if (namespace && typeof target === 'string') {
+          const x = `${namespace}.${target}`;
+          return _t(`${namespace}.${target}`, ...args);
+        }
+      }
+    };
+  }, []);
 
   useEffect(() => {
     i18n.addEventListener('languageChange', setResolvedLanguage);
