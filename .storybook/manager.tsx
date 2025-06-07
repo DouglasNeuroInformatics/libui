260184e43682ac10d9c3883b86bf84e8ac8641a4
joshunrau <contact@joshuaunrau.com>
2025-06-07 16:19:18 -0400

    refactor: remove manager

--- .storybook/manager.tsx
+++ .storybook/manager.tsx
@@ -1,17 +1,15 @@
 import React, { Fragment, useEffect } from 'react';
 
-import { LanguagesIcon, MoonIcon, SunIcon } from 'lucide-react';
+import { MoonIcon, SunIcon } from 'lucide-react';
 import { IconButton } from 'storybook/internal/components';
 import { addons, types } from 'storybook/manager-api';
 import { match } from 'ts-pattern';
 
 import { useTheme } from '../src/hooks/useTheme';
-import { useTranslation } from '../src/hooks/useTranslation';
 import theme from './theme';
 
 const Toolbar = React.memo(function Toolbar() {
   const [theme, setTheme] = useTheme();
-  const i18n = useTranslation();
 
   useEffect(() => {
     const iframe = document.getElementById('storybook-preview-iframe') as HTMLIFrameElement | null;
@@ -30,14 +28,6 @@ const Toolbar = React.memo(function Toolbar() {
           .with('dark', () => <SunIcon height={14} width={14} />)
           .with('light', () => <MoonIcon height={14} width={14} />)
           .exhaustive()}
-      </IconButton>
-      <IconButton
-        title="Language"
-        onClick={() => {
-          i18n.changeLanguage(i18n.resolvedLanguage === 'en' ? 'fr' : 'en');
-        }}
-      >
-        <LanguagesIcon height={14} width={14} />
       </IconButton>
     </Fragment>
   );
