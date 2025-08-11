a2c2a8ee762a9955b7eb06d19d76b4225dc78ba9
joshunrau <contact@joshuaunrau.com>
2025-08-11 19:42:25 -0400

    feat: add new addPendingDestructiveAction usage

--- src/hooks/useDestructiveAction/useDestructiveAction.ts
+++ src/hooks/useDestructiveAction/useDestructiveAction.ts
@@ -2,18 +2,24 @@ import { useCallback } from 'react';
 
 import { useDestructiveActionStore } from './useDestructiveActionStore';
 
-import type { DestructiveAction, DestructiveActionParams } from './useDestructiveActionStore';
+import type { DestructiveAction, DestructiveActionOptions, DestructiveActionParams } from './useDestructiveActionStore';
 
+export function useDestructiveAction(): (action: DestructiveAction, options?: DestructiveActionOptions) => void;
 export function useDestructiveAction<TArgs extends any[]>(action: DestructiveAction<TArgs>): (...args: TArgs) => void;
 export function useDestructiveAction<TArgs extends any[]>(
   params: DestructiveActionParams<TArgs>
 ): (...args: TArgs) => void;
 export function useDestructiveAction<TArgs extends any[]>(
-  arg: DestructiveAction<TArgs> | DestructiveActionParams<TArgs>
+  arg?: DestructiveAction<TArgs> | DestructiveActionParams<TArgs>
 ): (...args: TArgs) => void {
   const addPendingDestructiveAction = useDestructiveActionStore((store) => store.addPendingDestructiveAction);
   return useCallback(
     (...args: TArgs) => {
+      if (arg === undefined) {
+        const [action, options] = args as unknown as [DestructiveAction, DestructiveActionOptions | undefined];
+        addPendingDestructiveAction(action, options);
+        return;
+      }
       const { action, ...options } = typeof arg === 'function' ? { action: arg } : arg;
       addPendingDestructiveAction(() => action(...args), options);
     },
