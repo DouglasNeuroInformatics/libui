3b6e82c0f20b7de9ddf616576ca1f917d84d0a11
joshunrau <contact@joshuaunrau.com>
2025-10-07 17:17:32 -0400

    fix: increase animation duration by 100ms

--- src/components/Dialog/DialogOverlay.tsx
+++ src/components/Dialog/DialogOverlay.tsx
@@ -8,7 +8,7 @@ export const DialogOverlay = ({ className, ...props }: React.ComponentProps<type
   return (
     <Overlay
       className={cn(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80 duration-200',
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80 duration-300',
         className
       )}
       {...props}
