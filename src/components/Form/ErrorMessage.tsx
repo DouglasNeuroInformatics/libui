e5b905a4214d00bd94816bf28f0d76fda12d2c3a
joshunrau <contact@joshuaunrau.com>
2026-01-14 14:27:38 -0500

    fix: prevent icon in error message shrinking

--- src/components/Form/ErrorMessage.tsx
+++ src/components/Form/ErrorMessage.tsx
@@ -9,7 +9,7 @@ export const ErrorMessage: React.FC<{ className?: string; error?: null | string[
     <div className="flex flex-col gap-1.5">
       {error.map((message) => (
         <div className={cn('text-destructive flex w-full items-center text-sm font-medium', className)} key={message}>
-          <CircleAlertIcon className="mr-1" style={{ strokeWidth: '2px' }} />
+          <CircleAlertIcon className="mr-1 shrink-0" style={{ strokeWidth: '2px' }} />
           <span data-testid="error-message-text">{message}</span>
         </div>
       )) ?? null}
