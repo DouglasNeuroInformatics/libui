fdcb46e08e6ebe4917106aaaa073b9da09b20d5c
joshunrau <contact@joshuaunrau.com>
2026-01-16 20:36:12 -0500

    chore: remove Collapsible

--- src/components/Collapsible/Collapsible.tsx
+++
@@ -1,8 +0,0 @@
-import { Content, Trigger } from '@radix-ui/react-collapsible';
-
-import { CollapsibleRoot } from './CollapsibleRoot';
-
-export const Collapsible = Object.assign(CollapsibleRoot, {
-  Content,
-  Trigger
-});
