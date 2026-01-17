fce5d4c8ce9fb20b89878127c866fac1a89caecd
joshunrau <contact@joshuaunrau.com>
2026-01-17 10:46:39 -0500

    refactor: remove Drawer

--- src/components/Drawer/DrawerFooter.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-import { cn } from '@/utils';
-
-export const DrawerFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn('mt-auto flex flex-col gap-2 p-4', className)} {...props} />
-);
