2c5e1ec823501afd94fae37308124f205cb37900
David Roper <davidroper321@gmail.com>
2026-03-10 15:17:27 -0400

    feat: add story for input group

---
+++ src/components/InputGroup/InputGroup.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { SearchIcon } from 'lucide-react';
+
+import { InputGroup, InputGroupAddon, InputGroupInput } from './InputGroup.tsx';
+
+type Story = StoryObj<typeof InputGroup>;
+
+export default {
+  args: {
+    children: (
+      <InputGroup>
+        <InputGroupInput placeholder="Search..." />
+        <InputGroupAddon>
+          <SearchIcon />
+        </InputGroupAddon>
+      </InputGroup>
+    )
+  },
+  component: InputGroup,
+  tags: ['autodocs']
+} as Meta<typeof InputGroup>;
+
+export const Default: Story = {};
