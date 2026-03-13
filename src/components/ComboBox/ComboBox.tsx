d9de35f3c50bc927fb46102872f483748be6f083
David Roper <davidroper321@gmail.com>
2026-03-13 10:56:56 -0400

    fix: separate combobox use hook from object assign export

--- src/components/ComboBox/ComboBox.tsx
+++ src/components/ComboBox/ComboBox.tsx
@@ -1,38 +1,40 @@
 import * as React from 'react';
 
 import { Combobox as ComboboxPrimitive } from '@base-ui/react';
-import { ComboboxTrigger } from './ComboBoxTrigger.tsx';
-import { ComboboxContent } from './ComboBoxContent.tsx';
-import { ComboboxItem } from './ComboBoxItem.tsx';
-import { ComboboxList } from './ComboBoxList.tsx';
-import { ComboboxGroup } from './ComboBoxGroup.tsx';
-import { ComboboxLabel } from './ComboBoxLabel.tsx';
+
 import { ComboboxChip, ComboboxChips, ComboboxChipsInput } from './ComboBoxChips.tsx';
 import { ComboboxClear } from './ComboBoxClear.tsx';
 import { ComboboxCollection } from './ComboBoxCollection.tsx';
+import { ComboboxContent } from './ComboBoxContent.tsx';
 import { ComboboxEmpty } from './ComboBoxEmpty.tsx';
+import { ComboboxGroup } from './ComboBoxGroup.tsx';
 import { ComboboxInput } from './ComboBoxInput.tsx';
+import { ComboboxItem } from './ComboBoxItem.tsx';
+import { ComboboxLabel } from './ComboBoxLabel.tsx';
+import { ComboboxList } from './ComboBoxList.tsx';
 import { ComboboxSeparator } from './ComboBoxSeparator.tsx';
+import { ComboboxTrigger } from './ComboBoxTrigger.tsx';
 import { ComboboxValue } from './ComboBoxValue.tsx';
 
 function useComboboxAnchor() {
   return React.useRef<HTMLDivElement | null>(null);
 }
 
+export { useComboboxAnchor };
+
 export const ComboBox = Object.assign(ComboboxPrimitive.Root.bind(null), {
-  Content: ComboboxContent,
-  Anchor: useComboboxAnchor,
   Chip: ComboboxChip,
   Chips: ComboboxChips,
   ChipsInput: ComboboxChipsInput,
   Clear: ComboboxClear,
   Collection: ComboboxCollection,
+  Content: ComboboxContent,
   Empty: ComboboxEmpty,
   Group: ComboboxGroup,
   Input: ComboboxInput,
   Item: ComboboxItem,
-  List: ComboboxList,
   Label: ComboboxLabel,
+  List: ComboboxList,
   Separator: ComboboxSeparator,
   Trigger: ComboboxTrigger,
   Value: ComboboxValue
